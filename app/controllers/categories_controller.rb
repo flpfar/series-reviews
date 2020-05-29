82a3cb4c5acab1660f48718929e3811c5916136e
Felipe Rosa <flp.far@gmail.com>
2020-05-29 16:13:55 -0300

    Fix n+1 queries in categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
-    @articles = @category.articles.order(created_at: :desc)
+    @reviews = @category.reviews.includes(:votes).order(created_at: :desc)
   end
 end
