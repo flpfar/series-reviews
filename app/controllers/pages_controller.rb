3cbe721fc438eb302356cf6fd8519224cf24e0d6
Felipe Rosa <flp.far@gmail.com>
2020-05-29 16:20:15 -0300

    Fix featured_review in PagesController

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def index
-    @featured_review = Review.featured_review
+    @featured_review = Review.featured
     @categories = Category.ordered_by_priority
   end
 end
