6de4ad46c5bdd8d38091ed5ac437d3d4c2585989
Felipe Rosa <flp.far@gmail.com>
2020-05-29 13:06:57 -0300

    Add categories_ids and before_save to review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,11 +1,17 @@
 class Review < ApplicationRecord
+  attr_accessor :categories_ids
+
+  belongs_to :author, class_name: 'User'
   has_many :votes, dependent: :destroy
   has_and_belongs_to_many :categories do
     def <<(category)
       super category unless include?(category)
     end
   end
-  belongs_to :author, class_name: 'User'
+
+  before_save do
+    categories << Category.where(id: categories_ids) unless !categories_ids || categories_ids.empty?
+  end
 
   validates :author, :title, :text, presence: true
   validates :title, length: { in: 1..120 }, uniqueness: { scope: :author }
