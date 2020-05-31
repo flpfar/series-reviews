47378b683c1b67ecf0120b0f8add9840d747d68e
Felipe Rosa <flp.far@gmail.com>
2020-05-31 14:44:32 -0300

    Add author name in categories show

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,7 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find_by(id: params[:id])
     if @category
-      @reviews = @category.reviews.includes(:votes).order(created_at: :desc)
+      @reviews = @category.reviews.includes(:votes).includes(:author).order(created_at: :desc)
     else
       flash[:alert] = 'Category not found'
       redirect_to root_path
